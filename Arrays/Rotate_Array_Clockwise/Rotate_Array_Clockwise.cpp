8d13f0d3036a71ab37392ccc2f4f568bd97e94c4
Thangaraj N <thangarajn1992@gmail.com>
2021-06-24 10:18:01 +0530

    correction to rotate array

--- Arrays/Rotate_Array_Clockwise/Rotate_Array_Clockwise.cpp
+++ Arrays/Rotate_Array_Clockwise/Rotate_Array_Clockwise.cpp
@@ -3,22 +3,23 @@
 #include<string>
 using namespace std;
 
-void Rotate_Array_Counter_Clockwise(vector<int> &vec, int shift)
+void Rotate_Array_Clockwise(vector<int> &vec, int shift)
 {
     int size = vec.size();
     shift = shift % size;
     int start = 0, count = 0;
+    /* Handling Cyclic Dependencies */
     while(count < size)
     {
         int curr = start;
         int prev = vec[curr];
         do
         {
-            curr = (size - shift + curr) % size;
+            curr = (shift + curr) % size;
             int temp = prev;
             prev = vec[curr];
             vec[curr] = temp;
-            count++;/* code */
+            count++;
         }while(curr != start);
         start++;
     }
@@ -44,7 +45,7 @@ int main()
         cin >> vec[i];
     cout << "Enter the shifting size:" ;
     cin >> k;
-    Rotate_Array_Counter_Clockwise(vec, k);
+    Rotate_Array_Clockwise(vec, k);
     cout << "Array after shifting by " << k << " is " << print_vector_int(vec) << endl;
     return 1;
 }
